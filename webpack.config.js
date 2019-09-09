3294047aebd022518537dacc33e8e75a4e566f8e
huwewa <huwewa@gmail.com>
2019-09-09 18:12:02 +0800

    隐去暂时用不到的页面和布局

--- webpack.config.js
+++ webpack.config.js
@@ -150,7 +150,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
+                title: 'Ainoview',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
