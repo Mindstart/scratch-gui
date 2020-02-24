139c5ae17b9182c205dacd206215c3e39b8a64a3
huwewa <huwewa@gmail.com>
2020-02-24 16:08:26 +0800

    add+可以自定义安装位置

--- build/helper.js
+++ build/helper.js
@@ -13,12 +13,16 @@ function listener(req, res) {
 	req.setMaxListeners(300);
 	request = req.url;
 	if(request.indexOf("openIno") > 0){
+		console.log(`listener openIno`);
+		console.log(request);
 		request = request.split("/");
 		code = decodeURIComponent(request[2]);
 		createIno(code);
 		openIDE();
 	}
 	else if(request.indexOf("COM") > 0){
+		console.log(`listener COM`);
+		console.log(request);
 		request = request.split("/");
 		port = request[1];
 		code = decodeURIComponent(request[2]);
@@ -32,7 +36,30 @@ function listener(req, res) {
 }
 
 function createIno(code){
-	fs.writeFileSync('arduino/sketch/sketch.ino', code); 
+	console.log('createIno');
+	var currpath = process.cwd(); 
+	// Notice the double-backslashes on this following line
+	currpath = currpath.replace(/\\/g, '/');
+	// currpath = currpath.split("/package.nw");
+	// currpath = currpath[0];
+	var inopath = currpath + '/arduino/sketch/sketch.ino';
+	inopath = decodeURIComponent(inopath);
+	console.log(`createIno inopath ${inopath}`);
+
+	// var currpath = 'C:\\Users\\huwew\\Documents\\ainoview_test\\arduino\\sketch';
+	// var currpath = 'C:\\Users\\huwew\\Documents\\ainoview test\\arduino\\sketch';
+	// var currpath = 'C:\\Program Files (x86)\\AinoView_test_can_del\\nwjs-sdk-v0.32.2-win-x64\\package.nw\\arduino\\sketch';
+	// currpath = currpath.replace(/\\/g, '/');
+	// var inopath = currpath + '/sketch/sketch.ino';
+	// inopath = decodeURIComponent(inopath);
+	// console.log(`createIno inopath ${inopath}`);
+
+	fs.writeFileSync(inopath, code, (err) => {
+  		if (err) throw err;
+  		console.log('It\'s saved!');
+	}); 
+
+	// fs.writeFileSync('arduino/sketch/sketch.ino', code); 
 }
 
 function uploadSketch(port){
@@ -45,7 +72,25 @@ function uploadSketch(port){
 }
 
 function openIDE(){
-	const ide = child_process.exec('"arduino/arduino.exe" "arduino/sketch/sketch.ino"');
+	console.log(`openIDE ${process.cwd()}`);
+
+	// var arduinopath = 'C:\\Program Files (x86)\\Arduino';
+	// arduinopath = arduinopath.replace(/\\/g, '/');
+	// var exepath = arduinopath + '/arduino.exe'; 
+
+	var currpath = process.cwd(); 
+	// Notice the double-backslashes on this following line
+	currpath = currpath.replace(/\\/g, '/');
+	// var tmppath = currpath.split("/package.nw");
+	// var inopath = tmppath[0] + '/sketch/sketch.ino'; 
+	var inopath = currpath + '/arduino/sketch/sketch.ino'; 
+	var exepath = currpath + '/arduino/arduino.exe'; 
+
+	console.log(`openIDE inopath ${inopath}`);
+	console.log(`openIDE exepath ${exepath}`);
+
+	// const ide = child_process.exec('"arduino/arduino.exe" "arduino/sketch/sketch.ino"');
+	const ide = child_process.exec('"' + exepath + '"' + ' ' + '"' + inopath + '"');
 	ide.stderr.on('data', (data) => {
 		console.log(`stderr: ${data}`);
 	});
